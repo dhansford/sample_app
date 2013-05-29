ced690153d3bcd33ef0f1bcea1e43af2b4462405
unknown <default@winxp.(none)>
2013-05-29 17:14:58 +1000

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'ff6b969740917318a954eff2aa5d8ba1e670d3f2c8922d4ff6a57d8ac468f6208292c85e149c3e3897562f90ab35d14d4421dae4d9401e11d784ed8984e3bf17'
